a96fe41bc814abfcbb01ddd36837f9410e216375
temp <temp>
2022-11-23 21:01:39 +0900

    Feature1 if문 개선

--- Feature1.js
+++ Feature1.js
@@ -3,13 +3,15 @@ const Feature1 = function (rtm, channel, num) {
     console.log('(English)인사를 합니다.');
     rtm.sendMessage('Hello', channel);
     return 'Hello';
-  } if (num === 1) {
+  }else if (num === 1) {
     console.log('(Chinese)인사를 합니다.');
     rtm.sendMessage('Nǐ hǎo', channel);
     return 'Nǐ hǎo';
   }
-  console.log('(Español)인사를 합니다.');
-  rtm.sendMessage('Hola', channel);
-  return 'Hola';
+   else{
+	console.log('(Español)인사를 합니다.');
+  	rtm.sendMessage('Hola', channel);
+  	return 'Hola';
+}
 };
 module.exports = Feature1;
