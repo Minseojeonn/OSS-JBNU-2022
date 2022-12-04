621666799173b64021982eec748cc49140bd558e
minseo <raichal2000@naver.com>
2022-12-04 16:11:21 +0900

    <feature> : feature3 파일 생성 feature3.js 파일 생성하여 커밋. 딱히 없음.

---
+++ Feature3.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+const { Channel } = require('diagnostics_channel');
+
+const Feature3 = function (rtm, channel) {
+  
+  // console.log(data);
+  try {
+   
+  } catch (error) {
+    
+  }
+};
+
+module.exports = Feature3;
