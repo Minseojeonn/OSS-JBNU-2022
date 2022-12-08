6d6eaf21b0367e98a714065d484aba3feb11dc76
hyun4325 <danny4865@gmail.com>
2022-12-09 06:59:18 +0900

    F1 & F2& F4 conflict를 해결하기 위한 commit

--- index.js
+++ index.js
@@ -24,35 +24,38 @@ const rtm = new RTMClient(token);
 rtm.start();
 
 const { channel } = require('diagnostics_channel'); // eslint-disable-line
-const greeting = require('./greeting');
 const square = require('./square');
-const readdata = require('./read_data'); // eslint-disable-line
-// const Feature1 = require('./Feature1');  // eslint-disable-line
-// const Feature2 = require('./Feature2');   // eslint-disable-line
+const readdata = require('../OSS-JBNU-2022/read_data'); // eslint-disable-line
+const Feature1 = require('./Feature1');  // eslint-disable-line
+const Feature2 = require('./Feature2');   // eslint-disable-line
 // const Feature3 = require('./Feature3'); // eslint-disable-line
 const Feature4 = require('./Feature4'); // eslint-disable-line
 
 rtm.on('message', (message) => {
   const { channel } = message; // eslint-disable-line
   const { text } = message;
+
+  num = Math.floor(Math.random() * 3);
+  console.log(num);
   if (Number.isNaN(text)) {
     square(rtm, text, channel);
   } else if (regex.test(text)) {
     Feature2(rtm, channel, text);
   } else if (global.office.includes(text)) {
+    console.log('피처4 학과입력');
     Feature4(rtm, channel, text);
   } else {
     switch (text) {
       case '테스트를 시작한다.':
         break;
-      case 'hi':
-        greeting(rtm, channel);
+      case 'Hi':
+        Feature1(rtm, channel, num);
         break;
       case '학사일정':
         (async () => {
           rtm.sendMessage('안내 받을 날짜를 이야기해주세요. (예, 12/21)', channel);
           global.Channels[channel] = 0;
-          await Feature4(rtm, channel, text);
+          await Feature2(rtm, channel, text);
         })();
         break;
       case '학과 안내':
