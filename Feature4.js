2a1a69a51edbd97a7b99894b47dd7a25860e2ac8
minseo <raichal2000@naver.com>
2022-12-11 00:42:34 +0900

    <feature>  피처4: 레바테인 추가 레바테인 추가했습니다. 대소문자 띄어쓰기 해결했습니다.

--- Feature4.js
+++ Feature4.js
@@ -1,6 +1,7 @@
 /*eslint-disable*/
 const { checkPrime } = require('crypto');
 const { Channel } = require('diagnostics_channel');
+var levenshtein = require('fast-levenshtein');
 
 const Feature4 = function (rtm, channel, text) {
   try {
@@ -16,18 +17,40 @@ const Feature4 = function (rtm, channel, text) {
           check = i; 
           break;
         }
+        else if(global.modified_ofiice[i].includes(text.toUpperCase().replace(/ /g, ''))){
+          check = i; 
+          break;
+        }
       }
-      if(global.office.includes(text)) {
+      if(global.office.includes(text)) { //그냥 학과 있는지 확인
+        rtm.sendMessage(global.loc[check], channel);
+        return Promise.resolve('Success'); 
+      }
+      else if(global.modified_ofiice.includes(text.toUpperCase().replace(/ /g, ''))){ // 대문자, space 처리
         rtm.sendMessage(global.loc[check], channel);
         return Promise.resolve('Success');
-      }
-      else if(global.modified_ofiice.includes(text.toUpperCase().replace(/ /g, ''))){
-        rtm.sendMessage(global.loc[check], channel);
-        return Promise.resolve('Success');
-      } 
+      }  
       else { // no key in dict
-        rtm.sendMessage("잘못된 학과 이름입니다.", channel);
-        return Promise.resolve('wrong name')
+
+        // levenshtein distance control from npm install fast-levenshtein
+        let smallest_index = 0;
+        let smallest_value = levenshtein.get(text, global.office[0] , {useCollator: true});; //compare two string
+        for(var i = 1 ; i<global.office.length ; i++){
+          const levenshtein_result = levenshtein.get(text, global.office[i] , {useCollator: true}); //compare two string
+          if(smallest_value > levenshtein_result){
+            smallest_value = levenshtein_result;
+            smallest_index = i;
+            console.log(global.office.lenght , " ", global.office[i], levenshtein_result);
+          } 
+        }
+        if (smallest_value <= 7){ 
+          rtm.sendMessage(global.loc[smallest_index], channel);
+          return Promise.resolve('Success with levenshtein');
+        }
+        else{  
+          rtm.sendMessage("잘못된 학과 이름입니다.", channel);
+          return Promise.resolve('wrong name');
+        }
       }     
     } else { // first visit with date
       rtm.sendMessage('잘못된 접근입니다.', channel);
