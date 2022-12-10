bd31added69d3f27c0aa70c6159be5f3f235c9e0
hyun4325 <danny4865@gmail.com>
2022-12-10 13:56:37 +0900

    First commit about printing Feature3

--- Feature4.js
+++ Feature4.js
@@ -1,16 +1,33 @@
 /*eslint-disable*/
+const { checkPrime } = require('crypto');
 const { Channel } = require('diagnostics_channel');
 
 const Feature4 = function (rtm, channel, text) {
   try {
-    for (i in global.office) {
-      console.log(global.office[i].includes(text));
-      console.log(text);
-      if (global.office[i].includes(text)) {
-        rtm.sendMessage(global.loc[i], channel);
-        break;
+    let check = 0;
+    console.log('메시지 출력 완료');
+    if (text=='학과 안내') { // first visit with '학사일정'
+      global.Channels[channel] = 1;
+      return Promise.resolve('Success');
+    } else if (global.Channels[channel] === 1) {
+      global.Channels[channel] = 0;
+      for (i in global.office) {
+        if (global.office[i].includes(text)) {
+          check = i; 
+          break;
+        }
       }
-    }
+      if(global.office.includes(text)) {
+        rtm.sendMessage(global.loc[check], channel);
+        return Promise.resolve('Success');
+      } else { // no key in dict
+        rtm.sendMessage("잘못된 학과 이름입니다.", channel);
+        return Promise.resolve('wrong name')
+      }     
+    } else { // first visit with date
+      rtm.sendMessage('잘못된 접근입니다.', channel);
+      return Promise.resolve('wrong access');
+    }    
   } catch (error) {
     console.log('error!', error.data);
     return Promise.resolve('error');
